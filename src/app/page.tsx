a8beaaf1dadcb33ba621731e9bb83588f7275e17
Nikhil Sharma <83294513+thenikhilsharma@users.noreply.github.com>
2025-01-24 22:27:31 +0530

    horizontal carousel added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,29 @@
 "use client";
 
 import React from "react";
-import FeatureCard from "@/components/featureCard";
 import Hero from "@/components/Hero";
 import TrustedBy from "@/components/TrustedBy";
 import Navbar from "@/components/Navbar";
 import ContainerScroll from "@/components/ContainerScroll";
-import InfiniteTestimonials from '@/components/InfiniteTestimonials';
-import Footer from '@/components/Footer';
+import InfiniteTestimonials from "@/components/InfiniteTestimonials";
+import HorizontalCarousel from "@/components/HorizontalCarousel";
+import Footer from "@/components/Footer";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
-      <TrustedBy className="py-32"/>
+      <TrustedBy className="py-32" />
       <div className="bg-black">
         <ContainerScroll />
       </div>
-      <div className="flex gap-5 px-3 py-5 bg-black">
-        <FeatureCard />
-        <FeatureCard />
-        <FeatureCard />
-        <FeatureCard />
+      <div className="bg-neutral-800">
+        <HorizontalCarousel />
       </div>
-      <InfiniteTestimonials />
+      <div className="pt-28 bg-black">
+        <InfiniteTestimonials />
+      </div>
       <Footer />
     </div>
   );
---
+++ src/components/HorizontalCarousel.jsx
@@ -0,0 +1,86 @@
+import { motion, useTransform, useScroll } from "framer-motion";
+import { useRef } from "react";
+
+const HorizontalScrollCarousel = () => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
+
+  return (
+    <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
+      <div className="sticky top-0 flex h-screen items-center overflow-hidden">
+        <motion.div style={{ x }} className="flex gap-20">
+          {cards.map((card) => {
+            return <Card card={card} key={card.id} />;
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default HorizontalScrollCarousel;
+
+const Card = ({ card }) => {
+  return (
+    <div
+      key={card.id}
+      className="group relative h-[450px] w-[450px] overflow-hidden bg-black"
+    >
+      <div
+        style={{
+          backgroundImage: `url(${card.url})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
+      ></div>
+      <div className="absolute inset-0 z-10 grid place-content-center">
+        <p className="bg-gradient-to-br from-white/20 to-white/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">
+          {card.title}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const cards = [
+  {
+    url: "/imgs/abstract/1.jpg",
+    title: "Title 1",
+    id: 1,
+  },
+  {
+    url: "/imgs/abstract/1.jpg",
+    title: "Title 2",
+    id: 2,
+  },
+  {
+    url: "/imgs/abstract/1.jpg",
+    title: "Title 3",
+    id: 3,
+  },
+  {
+    url: "/imgs/abstract/1.jpg",
+    title: "Title 4",
+    id: 4,
+  },
+  {
+    url: "/imgs/abstract/1.jpg",
+    title: "Title 5",
+    id: 5,
+  },
+  {
+    url: "/imgs/abstract/1.jpg",
+    title: "Title 6",
+    id: 6,
+  },
+  {
+    url: "/imgs/abstract/1.jpg",
+    title: "Title 7",
+    id: 7,
+  },
+];
